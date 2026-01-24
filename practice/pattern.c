e16f7697ccb1e5722d7ed7516e2c9d88fe83516e
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-01-24 21:46:41 +0545

    half pyramid

--- practice/pattern.c
+++ practice/pattern.c
@@ -1,19 +1,50 @@
+// #include <stdio.h>
+// int main() {
+//     int i, j, k, space, star=1, rows;
+//     rows=5;
+//     space= rows-1;
+//     for(i=0; i<rows; i++) {
+//         for(k=0; k<space; k++) {
+//             printf("  ");
+//         }
+//         space--;
+//         for(j=0; j<star; j++) {
+//             printf("* ");
+//         }
+//         star+=2;
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+///
+// #include <stdio.h>
+// int main() {
+//     int i, j, k, star=1, rows;
+//     rows =5;
+//   for(int a=0; a<2; a++) {
+//       for(i=0; i<rows; i++) {
+//         for(j=0; j<=i; j++) {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//   }
+// }
+
 #include <stdio.h>
 int main() {
-    int i, j, k, space, star=1, rows;
-    rows=5;
-    space= rows-1;
+    int i, j, k, one, rows;
+    rows=9;
+    one =1;
     for(i=0; i<rows; i++) {
-        for(k=0; k<space; k++) {
-            printf("  ");
+        for(j=0; j<i; j++) {
+            printf("%d ", one);
         }
-        space--;
-        for(j=0; j<star; j++) {
-            printf("* ");
-        }
-        star+=2;
         printf("\n");
     }
-    printf("       |  |    ");
-    return 0;
 }
