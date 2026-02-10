9219afc2121526a3aefff637319e5bbd22852909
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-02-11 00:16:45 +0545

    hell0

--- lab6/revstr.c
+++ lab6/revstr.c
@@ -5,12 +5,13 @@ int main() {
 
     printf("Enter a string: ");
     scanf("%s", str);
-    // fgets(str, sizeof(str), stdin);
+      p=str;
+
     while (*p != '\0') {
         len++;
         p++;
-    }
-    p=str;
+     }
+    // p=str;
 
     printf("Reversed string: ");
     for (i = len - 1; i >= 0; i--) {
