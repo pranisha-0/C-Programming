72f014ef3fd46bdcac102d0f140246ced5a19187
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-03-10 13:27:10 +0545

    mul

--- practice/array.c
+++ practice/array.c
@@ -77,37 +77,81 @@
 //             }
 //             printf("\n");
 //         }
-
 // }
 
 //matrix add
+// #include <stdio.h>
+// int main() {
+//     int a[10][10],b[10][10], c[10][10], row, col;
+//         printf("row and column: ");
+//         scanf("%d %d", &row, &col);
+//         printf("enter elements of a: \n");
+//         for(int i=0; i<row; i++) {
+//             for(int j=0; j<col; j++) {
+//             scanf("%d", &a[i][j]);
+//             }
+//         }
+//         printf("enter elements of b: \n");
+//         for(int i=0; i<row; i++) {
+//             for(int j=0; j<col; j++) {
+//             scanf("%d", &b[i][j]);
+//             }
+//         }
+//         for(int i=0; i<row; i++) {
+//             for(int j=0; j<col; j++) {
+//            c[i][j] = a[i][j] + b[i][j];
+//             }
+//         }
+//         printf("sum of matrix is: \n");
+//         for(int i=0; i<row; i++) {
+//             for(int j=0; j<col; j++) {
+//             printf("  %d ", c[i][j]);
+//             }
+//             printf("\n");
+//         }
+//     }
+
+//matrix multiply
 #include <stdio.h>
 int main() {
-    int a[10][10],b[10][10], c[10][10], row, col;
-        printf("row and column: ");
-        scanf("%d %d", &row, &col);
-        printf("enter elements of a: \n");
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
+    int a[10][10], b[10][10], c[10][10], rowA, colA, rowB, colB;
+    int i, j, k, sum=0;
+    printf("Enter no of rows and cols for matA: ");
+    scanf("%d %d", &rowA, &colA);
+    printf("Enter no of rows and cols for matB: ");
+    scanf("%d %d", &rowB, &colB);
+    
+   if(colA == rowB) {
+     printf("Reading MatA: \n");
+    for(i=0; i<rowA; i++) {
+        for(j=0; j<colA; j++) {
             scanf("%d", &a[i][j]);
-            }
-        }
-        printf("enter elements of b: \n");
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-            scanf("%d", &b[i][j]);
-            }
-        }
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-           c[i][j] = a[i][j] + b[i][j];
-            }
-        }
-        printf("sum of matrix is: \n");
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-            printf("  %d ", c[i][j]);
-            }
-            printf("\n");
         }
     }
+    printf("Reading MatB: \n");
+    for(i=0; i<rowB; i++) {
+        for(j=0; j<colB; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("MatA * MatB : \n");
+    for(i=0; i<rowA; i++) {
+        for(j=0; j<colB; j++) {
+            for(k=0; k<colA; k++) {
+                sum = sum + a[i][k]*b[k][j];
+            }
+            c[i][j]=sum;
+            sum = 0;
+        }
+    }
+    for(i=0; i<rowA; i++) {
+        for(j=0; j<colB; j++) {
+            printf("  %d ", c[i][j]);
+        }
+        printf("\n");
+    }
+   }
+   else{
+    printf("The order is wrong.\n");
+   }
+}
