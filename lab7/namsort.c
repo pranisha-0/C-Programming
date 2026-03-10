57ef994080eafd35b56493b5387bb021862905d3
Pranisha Shrestha <082bel021@ioepc.edu.np>
2026-03-10 14:06:32 +0545

    l

--- lab7/namsort.c
+++ lab7/namsort.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
-void swap(char *a[], int i, int j) {
-    char *temp;
-    temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
-}
+#include <string.h>
+
+void sort(char a[][20], int n);
 
 int main() {
-    
+    char a[5][20]; // 5 names, max length 20
+    int i;
+
+    printf("Enter the list of 5 names:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%s", a[i]); // Simpler alternative to gets()
+    }
+
+    sort(a, 5);
+
+    printf("\nThe sorted names are:\n");
+    for(i = 0; i < 5; i++) {
+    puts(a[i]);
+    }
+    return 0;
 }
+
+void sort(char a[][20], int n) {
+    int i, j;
+    char temp[20];
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(strcmp(a[i], a[j]) > 0) { // Alphabetical comparison
+                strcpy(temp, a[i]);
+                strcpy(a[i], a[j]);
+                strcpy(a[j], temp);
+
+            }
+        }
+    }
+} flowcrt
